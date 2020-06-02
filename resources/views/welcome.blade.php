c8296740f272adbebd2cc25266f360ef68665d95
Edgar Reyes <ereyes@occ.com.mx>
2020-06-01 20:55:24 -0500

    se modifica inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,19 +81,9 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    INSTAGREM
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <img src="https://concepto.de/wp-content/uploads/2018/09/fotografia1-e1537978312239.jpg" class="card-img-top" alt="image">
             </div>
         </div>
     </body>
